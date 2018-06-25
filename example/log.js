b625c50f1a3332977b1e8ccb1428c36d33fa3d4e
Chen Chen <chenc5@andrew.cmu.edu>
2018-06-25 00:08:26 -0400

    updated log.js

--- example/log.js
+++ example/log.js
@@ -1,14 +1,18 @@
 var winston = require('winston');
-var winstonRemoteTransport = require('../index').Transport;
+var winstonRemoteTransport = require('winston-remote').Transport;
+
+winston.exitOnError = false;//
 
 var logger = winston.createLogger({
     transports: [
-        new (winstonRemoteTransport)({
+        new winston.transports.Console(),
+        new winstonRemoteTransport({
             host: '127.0.0.1', // Remote server ip
             port: 9003 // Remote server port
         })
     ],
-    exitOnError: false
+    exitOnError: false,
+    silent: false
 });
 
 setInterval(() => {
